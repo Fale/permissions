0276ee17a7e6474a4175b8b00e41be6a0da821db
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-17 01:03:17 +0200

    First version (cheap coding)

---
+++ index.php
@@ -0,0 +1,35 @@
+<style>
+.yes {background:green;}
+.no {background:red;}
+</style>
+
+<?php
+$allUsers = Array();
+
+exec('grep -v :$ /etc/group', $lines);
+
+foreach ($lines as $key => $line) {
+    $temp = explode(':', $line);
+    $users = explode(',', $temp[3]);
+    foreach($users as $user){
+        $allUsers[$user] = true;
+        $groups[$temp[0]][$user] = true;
+    }
+}
+
+echo "<table style='width:100%;' border='1px solid black;'>";
+echo "<th>";
+foreach ($groups as $group => $value)
+    echo "<td>$group</td>";
+echo "</th>";
+foreach ($allUsers as $allUser => $n){
+    echo "<tr>";
+    echo "<td>$allUser</td>";
+    foreach ($groups as $group => $users)
+        if( $users[$allUser])
+            echo "<td class='yes'></td>";
+        else
+            echo "<td class='no'></td>";
+    echo "</tr>";
+}
+echo "</table>";
