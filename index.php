629d081bc4e3f8cc2e20538a6f09ef7158047567
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-17 02:42:28 +0200

    Improve code and look

--- index.php
+++ index.php
@@ -18,16 +18,18 @@ foreach ($lines as $key => $line) {
     }
 }
 
+$width = 100 / (count($groups) + 1);
+
 echo "<table style='width:100%;' border='1px solid black;'>";
 echo "<th>";
 foreach ($groups as $group => $value)
-    echo "<td>$group</td>";
+    echo "<td width='$width%'>$group</td>";
 echo "</th>";
 foreach ($allUsers as $allUser => $n){
     echo "<tr>";
     echo "<td>$allUser</td>";
     foreach ($groups as $group => $users)
-        if ($users[$allUser])
+        if (array_key_exists($allUser, $users))
             echo "<td class='yes'></td>";
         else
             if (array_key_exists($allUser, $groups['sudo']))
