d141ba918585843c76e30b62170d0f53739016d8
root <root@lorenzo-desktop.(none)>
2013-04-17 14:47:22 +0200

    something fancy for the table

--- index.php
+++ index.php
@@ -20,11 +20,12 @@ foreach ($lines as $key => $line) {
 
 $width = 100 / (count($groups) + 1);
 
-echo "<table style='width:100%;' border='1px solid black;'>";
-echo "<th>";
+echo "<table id='myTable' class='tablesorter' style='width:100%;'>";
+echo "<thead><tr><th>";
 foreach ($groups as $group => $value)
-    echo "<td width='$width%'>$group</td>";
-echo "</th>";
+    echo "<td  width='$width%'>$group</td>";
+echo "</th></tr>";
+echo "<tbody>";
 foreach ($allUsers as $allUser => $n){
     echo "<tr>";
     echo "<td>$allUser</td>";
@@ -38,4 +39,16 @@ foreach ($allUsers as $allUser => $n){
                 echo "<td class='no'></td>";
     echo "</tr>";
 }
+echo "</tbody>";
 echo "</table>";
+
+echo "<link rel='stylesheet' href='js/blue/style.css'>";
+echo "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
+echo "<script type='text/javascript' src='js/jquery.tablesorter.min.js'></script> 
+<script>
+$(document).ready(function() 
+    { 
+        $('#myTable').tablesorter(); 
+    } 
+);
+</script>";
